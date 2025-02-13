9544322eb99e869b411b7afb8fea5d1bc27b8f16
RESHMA <RESHMA@192.168.0.38>
2025-02-13 14:54:53 +0530

    Saved ones

--- AutomationCourse/src/test/java/seleniumBasics/MultipleWindowHandling.java
+++ AutomationCourse/src/test/java/seleniumBasics/MultipleWindowHandling.java
@@ -7,44 +7,42 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 public class MultipleWindowHandling extends Base {
-	
-	public void handlingMultipleWindow()
-	{
+
+	public void handlingMultipleWindow() {
 		driver.navigate().to("https://demo.guru99.com/popup.php");
-		
-		String parentWindowHandleId=driver.getWindowHandle();
+
+		String parentWindowHandleId = driver.getWindowHandle();
 		System.out.println(parentWindowHandleId);
-		
-		WebElement clickField=driver.findElement(By.xpath("//a[text()='Click Here']"));
+
+		WebElement clickField = driver.findElement(By.xpath("//a[text()='Click Here']"));
 		clickField.click();
-		
-		Set<String> handleIds=driver.getWindowHandles();//get all window handles of all childs
-		Iterator<String> values =handleIds.iterator();
-		while(values.hasNext())
-		{
-			String currentId=values.next();
-			//not same as parent id
-			if(!currentId.equals(parentWindowHandleId))
-			{
+
+		// set to get all window handles of all childs in that webpage
+		Set<String> handleIds = driver.getWindowHandles();
+		Iterator<String> values = handleIds.iterator();
+		while (values.hasNext()) {
+			String currentId = values.next();
+			// not same as parent id
+			if (!currentId.equals(parentWindowHandleId)) {
 				driver.switchTo().window(currentId);
-				WebElement email=driver.findElement(By.xpath("//input[@name='emailid']"));
+
+				// go to that particular child
+				WebElement email = driver.findElement(By.xpath("//input[@name='emailid']"));
 				email.sendKeys("treesa@gmail.com");
-				
-				WebElement submit=driver.findElement(By.xpath("//input[@name='btnLogin']"));
+
+				// to submit
+				WebElement submit = driver.findElement(By.xpath("//input[@name='btnLogin']"));
 				submit.click();
 			}
-			
 		}
-		
 	}
 
 	public static void main(String[] args) {
-		MultipleWindowHandling multiple=new MultipleWindowHandling();
-		
+		MultipleWindowHandling multiple = new MultipleWindowHandling();
+
 		multiple.initializeBrowser();
 		multiple.handlingMultipleWindow();
-		//multiple.driverCloseAndQuit();
-		
+		// multiple.driverCloseAndQuit();
 
 	}
 
